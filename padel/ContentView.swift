c04e6f7697f7bcdc0cb63cbc59a94c2b9261e20d
Vadim Burkin <mailboxofvadim@gmail.com>
2026-08-31 16:24:29 +0300

    Initial Commit

---
+++ padel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  padel
+//
+//  Created by Vadim on 31.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ padel/padelApp.swift
@@ -0,0 +1,17 @@
+//
+//  padelApp.swift
+//  padel
+//
+//  Created by Vadim on 31.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct padelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
